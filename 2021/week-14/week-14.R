150618e5fe14fe6d0d603d0c11923fccdbde0a6f
doehm <danieloehm@gmail.com>
2021-04-05 13:50:46 +1000

    week 14 done

--- 2021/week-14/week-14.R
+++ 2021/week-14/week-14.R
@@ -2,27 +2,14 @@
 library(tidyverse)
 library(janitor)
 library(glue)
-library(snakecase)
-library(lubridate)
 library(extrafont)
-library(cowplot)
-library(ggforce)
-library(ggfx)
+library(patchwork)
+library(ggtext)
 extrafont::loadfonts(quiet = TRUE)
 
 #### data ####
 tt <- tidytuesdayR::tt_load(2021, week = 14)
 tt
-
-tt$allShades %>%
-  count(brand, sort = TRUE)
-
-tt$allShades %>%
-  filter(hue < 50) %>% {
-  # filter(brand == "Clinique") %>% {
-    ggplot(., aes(x = hue, y = lightness)) +
-    geom_point(colour = .$hex, size = 2)
-  }
 
 a <- 0.9
 b <- 0.45
@@ -48,14 +35,16 @@ df <- tt$allShades %>%
 n <- df %>%
   count(huec, lightc)
 
-x <- tt$allShades %>%
-  filter(hue < 50) %>%
-  filter(brand == "Maybelline")
+df1 <- df %>%
+  distinct(huec, lightc) %>%
+  arrange(huec, lightc) %>%
+  mutate(id = 1:n())
 
-kmeans(x[,c("hue", "sat", "lightness")], 6)$centers
+df <- df %>%
+  left_join(df1)
 
 #### colours ####
-bg <- "white"
+bg <- "#c38e70"
 font <- "grey10"
 
 #### fonts ####
@@ -63,23 +52,95 @@ ft <- "Amatic"
 ftc <- "Amatic SC"
 
 #### palettes ####
+make_palette <- function(.brand, n = 6, a = 0,  hex = FALSE) {
+
+  pal_df <- tt$allShades %>%
+    filter(hue < 50) %>%
+    filter(brand == .brand)
+
+  fills <- colorRampPalette(sort(pal_df$hex))(n)
+
+  df <- pal_df %>%
+    mutate(
+      x = (1:n()) + a,
+      xend = x+1,
+      y = 0,
+      yend = 1,
+      huec = 10,
+      lightc = 10
+    ) %>%
+    filter(x <= n + a)
+    # ggplot() +
+    # geom_rect(aes(xmin = x, xmax = xend, ymin = y, ymax = yend), fill = fills) +
+    # theme_void()
+
+  list(
+    data = df,
+    pal = fills
+  )
+
+}
 
 
 #### plot ####
-df %>%
+main <- df %>%
   ggplot(aes(x = huec, y = lightc)) +
-  geom_rect(aes(xmin = huec, xmax = huec_end, ymin = lightc, ymax = lightc_end), fill = NA, colour = font) +
-  geom_text(data = n, mapping = aes(huec+b, lightc+b, label = n), family = ft, size = 8, colour = font) +
+  geom_rect(aes(xmin = huec, xmax = huec_end, ymin = lightc, ymax = lightc_end), fill = "white", colour = font) +
+  geom_text(data = df, mapping = aes(huec+b, lightc+b, label = id), family = ft, size = 8, colour = font) +
+  geom_text(aes(x = 21, y = 21, label = str_wrap("High variation in hue and lightness", 20)), family = ftc, size = 5, lineheight = 0.8) +
+  geom_text(aes(x = 21, y = 6, label = str_wrap("High variation in hue", 20)), family = ftc, size = 5, lineheight = 0.8) +
+  geom_text(aes(x = 5, y = 21, label = str_wrap("High variation in lightness", 20)), family = ftc, size = 5, lineheight = 0.8) +
+  # geom_rect(data = cl$data, mapping = aes(xmin = x, xmax = xend, ymin = 15, ymax = 15.9), fill = cl$pal) +
+  # geom_text(data = n, mapping = aes(27.5, 15+b, label = "Clinique"), family = ft, size = 8, colour = font, hjust = 1) +
+  # scale_fill_gradientn(colours = pals[[1]]) +
   theme_void() +
   theme(
     text = element_text(family = ft, colour = font),
     plot.title = element_text(family = ft, size = 36),
-    plot.background = element_rect(fill = bg),
-    axis.text = element_text(size = 16),
-    axis.title = element_text(size = 20),
+    plot.background = element_rect(fill = bg, colour = NA),
+    # axis.text = element_text(size = 16),
+    axis.title = element_text(size = 22),
+    plot.margin = margin(t = 30,  b = 30, l = 30, r = 30),
+    axis.title.y = element_text(angle = 90)
   ) +
   labs(
-    title = "Variation of Hue and Lightness in Foundation Colour Palettes"
+    x = "variation in Hue",
+    y = "variation in Lightness"
   ) +
-  ggsave("./2021/week-14/make-up.png")
+  coord_cartesian(clip = "off")
+
+g_title <- ggplot() +
+  geom_text(aes(x = 0, y = 1, label = str_wrap("Anastasia Beverly Hills, Morphe and Maybelline all have high variation in Lightness and hue", 20)), family = ft, size = 13, lineheight = 0.8, hjust = 0) +
+  geom_text(aes(x = 0, y = 0, label = str_wrap("The variation in hue and lightenss was calculated for each brand with more than 30 colours. Those in the top right hand corner have high variation in both lightness and hue, therefore covering more of the spectrum", 35)), family = ftc, size = 7, lineheight = 0.8, hjust = 0) +
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = bg, colour = NA),
+    text = element_text(family = ft, colour = font),
+    plot.margin = margin(t = 30,  b = 10, l = 30, r = 10)
+  ) +
+  xlim(c(0, 1)) +
+  coord_cartesian(clip = "off") +
+  ylim(c(-0.5, 1.5))
+
+g_legend <- df %>%
+  arrange(id) %>%
+  mutate(
+    y = (1:68 - 1) %% 23 + 1,
+    x = cumsum(y == 1)
+  ) %>%
+  ggplot(aes(x, -y, label = paste(id, "  ", brand))) +
+  geom_text(family = ft, size = 7, colour = font, hjust = 0) +
+  theme_void() +
+  theme(
+    plot.background = element_rect(fill = bg, colour = NA),
+    text = element_text(family = ft, colour = font),
+    plot.margin = margin(t = 30,  b = 10, l = 30, r = 10)
+  ) +
+  xlim(c(1, 4))
+
+g_title +
+  main +
+  g_legend +
+  plot_layout(widths = c(1, 2, 3)) +
+  ggsave("./2021/week-14/make-up.png", height = 8, width = 21, dpi = 600)
 
