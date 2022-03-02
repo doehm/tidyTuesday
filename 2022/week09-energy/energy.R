b23dc6926d5549db120e762d8ac0f4a9e70f40b5
doehm <danieloehm@gmail.com>
2022-03-03 08:40:42 +1100

    ADD week 9 energy

---
+++ 2022/week09-energy/energy.R
@@ -0,0 +1,92 @@
+# https://github.com/rfordatascience/tidytuesday
+
+source("scripts/startup.R")
+
+log_file <<- "2022/week09-energy/log.txt"
+
+# load data ---------------------------------------------------------------
+
+stations <- readr::read_csv('https://raw.githubusercontent.com/rfordatascience/tidytuesday/master/data/2022/2022-03-01/stations.csv') |>
+  clean_names()
+
+# fonts and palettes ------------------------------------------------------
+
+bg <- "white"
+
+ft_text <- "barlow"
+ft_title <- ft_text
+
+# wrangle -----------------------------------------------------------------
+
+df_base_0 <- stations |>
+  time_log() |>
+  group_by(state) |>
+  summarise(p = sum(fuel_type_code == "ELEC")/n())
+
+# create the fill charts and merge with the bolts
+for(k in 1:nrow(df_base)) {
+  tibble(x = 0, y = df_base$p[k]) |>
+    ggplot(aes(x, y)) +
+    geom_col(fill = "#219ebc") +
+    ylim(c(0, 1)) +
+    theme_void() +
+    theme(plot.background = element_rect(fill = "black"), plot.margin = margin(0, -100, -100, -100)) +
+    ggsave(glue("2022/week09-energy/bolts/{df_base$state[k]}.png"), height = 4, width = 3)
+
+  base <- image_read('C:/Users/Dan/Downloads/fontawesome/lightning-bolt.png') |>
+    image_resize("220x180")
+  measure <- image_read(glue("2022/week09-energy/bolts/{df_base$state[k]}.png")) |>
+    image_resize("220x180")
+
+  image_composite(base, measure, "plus") |>
+    image_write(glue("2022/week09-energy/bolts/{df_base$state[k]}.png"))
+}
+
+df_base <- df_base_0 |>
+  mutate(
+    code = state,
+    info_graph = glue("<img src='2022/week09-energy/bolts/{state}.png' width = 35 height = 50>")
+    )
+
+# titles ------------------------------------------------------------------
+
+title <- "Alternative Fuel Stations"
+subtitle <- str_rich_wrap("
+The proportion of fuel stations offering <strong><span style='color:#219ebc'>electric charging facilities</span></strong> is
+very high in the East and West of the US but lacking in the central states.
+", 100)
+caption <- glue("Graphic: {get_icon('twitter', 10)} @danoehm / Source: US DOT / Code: {get_icon('github', 10)} doehm/tidytuesday #rstats #tidytuesday")
+
+# plot --------------------------------------------------------------------
+
+df_base |>
+  time_log() |>
+  ggplot() +
+  geom_richtext(aes(0, 0, label = info_graph), label.colour = NA, fill = NA) +
+  facet_geo(~code, grid = us_state_grid1) +
+
+  # theme and scales and labs
+  scale_fill_manual(values = pal) +
+  scale_colour_manual(values = pal) +
+  labs(
+    title = title,
+    subtitle = subtitle,
+    caption = caption,
+    fill = fill,
+    colour = colour
+  ) +
+  theme_void() +
+  theme(
+    text = element_text(colour = dark),
+    plot.background = element_rect(fill = bg, colour = NA),
+    plot.title = element_text(hjust = 0.5, family = ft_title, size = 200, face = "bold"),
+    plot.subtitle = element_markdown(hjust = 0.5, family = ft_text, size = 64, lineheight = 0.35, margin = margin(b = 10), halign = 0),
+    plot.caption = element_markdown(hjust = 0.5, family = ft_text, size = 36, lineheight = 0.35, margin = margin(t = 20)),
+    plot.margin = margin(t = 20, r = 20, b = 20, l = 20),
+    legend.title = element_text(family = ft_text, size = 36, lineheight = 0.25, face = "bold"),
+    legend.text = element_text(family = ft_text, size = 36, face = "bold"),
+    legend.position = "bottom",
+    strip.text = element_text(family = ft_text, size = 60, margin = margin(b = 5), face = "bold")
+  ) +
+  ggsave("2022/week09-energy/energy.png", height = 11.5, width = 16)
+
--- scripts/startup.R
+++ scripts/startup.R
@@ -11,6 +11,8 @@ library(colorspace)
 library(snakecase)
 library(ggbump)
 library(geofacet)
+library(crayon)
+library(magick)
 
 # rich wrap ---------------------------------------------------------------
 
@@ -59,9 +61,22 @@ time_log <- function(df, ...) {
 }
 
 get_time <- function(...) {
-  read_table(log_file, col_names = c("date", "time")) |>
+
+  stamp <- str_replace(log_file, ".txt", "-posted.txt")
+  if(file.exists(stamp)) {
+    posted <- read_table(str_replace(log_file, ".txt", "-posted.txt"), col_names = c("date", "time"), col_types = cols())
+    posted <- ymd_hms(paste(posted$date, posted$time))
+
+    cat(cyan(glue("\n\nPosted at {posted}\n\n\n")))
+
+  } else {
+    posted <- ymd_hms("2999-01-01 00:00:00")
+  }
+
+  read_table(log_file, col_names = c("date", "time"), col_types = cols()) |>
+    mutate(date_time = ymd_hms(paste(date, time))) |>
+    filter(date_time <= posted) |>
     mutate(
-      date_time = ymd_hms(paste(date, time)),
       duration = difftime(date_time, lag(date_time), units = "mins"),
       duration = replace_na(duration, 0),
       duration = pmin(duration, 10)
@@ -70,9 +85,55 @@ get_time <- function(...) {
     mutate(hours = glue("{floor(total/60)}h{round(60*(total/60 - floor(total/60)))}m"))
 }
 
+posted <- function(...) {
+  stamp <- str_replace(log_file, ".txt", "-posted.txt")
+  write_lines(Sys.time(), file = stamp, append = TRUE)
+}
+
 # min max -----------------------------------------------------------------
 
 min_max <- function(x, a, b) {
   (b - a) * (x - min(x)) / (max(x) - min(x)) + a
 }
+
+
+# get icon ----------------------------------------------------------------
+
+get_icon <- function(name, size = 50, fill = NULL) {
+  img <- glue('C:/Users/Dan/Downloads/fontawesome/{name}.png')
+
+  if(!is.null(fill)) {
+
+    if(name == "github") {
+      pars <- list(
+        bg = list(col = fill$bg, point = "+1+1"),
+        img = list(col = fill$img, point = "+900+200")
+      )
+    }
+
+    if(name == "twitter") {
+      pars <- list(
+        bg = list(col = fill$bg, point = "+1+1"),
+        img = list(col = fill$img, point = "+1200+200")
+      )
+    }
+
+    if(name == "lightning-bolt") {
+      pars <- list(
+        bg = list(col = fill$bg, point = "+1+1"),
+        bg2 = list(col = fill$bg2, point = "+1400+100"),
+        img = list(col = fill$img, point = "+700+900")
+      )
+    }
+
+    x <- image_read(img)
+    if(!is.null(fill$bg)) x <- image_fill(x, color = pars$bg$col, point = pars$bg$point, fuzz = 0)
+    if(!is.null(fill$bg2)) x <- image_fill(x, color = pars$bg2$col, point = pars$bg2$point, fuzz = 0)
+    if(!is.null(fill$img)) x <- image_fill(x, color = pars$img$col, point = pars$img$point, fuzz = 0)
+    img <- str_replace(img, ".png", "-fill.png")
+    image_write(x, img)
+  }
+  glue("<img src='{img}' height = {size} width = {size}>")
+}
+
 
